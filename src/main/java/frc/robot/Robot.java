72ddff250353ed757722ac6dad348a75dc2a7f73
Jocelyn <unikittyfan@gmail.com>
2020-01-18 12:13:17 -0800

    wew
    HAMBORGER

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-//Cheeseburger
+//Cheeseburger hamburger
   private final Spark mRightMaster = new Spark(0);
   private final Spark mRightSlave = new Spark(1);
   private final Spark mLeftMaster = new Spark(2);
